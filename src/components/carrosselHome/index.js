ce08d3c2fb6443440631ac0db4e9068cbbf4fe1a
lucapcf <lucapcf@gmail.com>
2023-12-24 19:19:40 -0300

    .

--- src/components/carrosselHome/index.js
+++ src/components/carrosselHome/index.js
@@ -30,76 +30,73 @@ export default function CarrosselHome(...posts) {
 
   return (
     <>
-      <section className={styles.carrossel}>
-        <h3 className={styles.titleSection}>
-          NOVIDADES
-        </h3>
-        <Swiper
-      
-            style={{
-              "--swiper-theme-color":"#F2C12E",
-              "--swiper-pagination-color": "#F2C12E",
-              "--swiper-pagination-bullet-inactive-color": "#999999",
-              "--swiper-pagination-bullet-inactive-opacity": "1",
-              "--swiper-pagination-bullet-size": "16px",
-              "--swiper-pagination-bullet-border-radius": "0",
-              "--swiper-pagination-bullet-width": "40px",
-              "--swiper-pagination-bullet-height": "6px",
-              "--swiper-pagination-bullet-horizontal-gap" :" 15px",
-              "--swiper-navigation-gap" : "100px"
-            }
-            }  
-            autoplay ={{
-              delay: 5000,
-              disableOnInteraction: false,
-            }}
-            modules={[Autoplay]}
-            grabCursor={true}
-            pagination={{
-              clickable:true
-            }}
-
-            navigation={{  //configurações do navi
-              enabled: false
-            }}
-
-            breakpoints={{
-              1281: {  //acima de 800px, aumenta os slides por visualização para dois, e ativa o navigation (as setinhas)
-                
-                navigation: {
-                  enabled: true  
-                }}}}
-          
-            
-            
-            className={styles.swiperContainer}
-            
-
-        >
-          {data.map((item)=> (
-            
-          <SwiperSlide key={item.titulo} className={styles.swiperIndi}>
-            <CardPosts 
-              fileName={item.fileName}
-              imagem={item.imagemCapa}
-              titulo={item.titulo}
-              data={item.data}
-              previa={item.previa}
-              autor={item.autor}
-            />
-            <div className={styles.margin}>
-
-            </div>
-          </SwiperSlide>))
+    <section className={styles.carrossel}>
+      <h3 className={styles.titleSection}>
+        NOVIDADES
+      </h3>
+      <Swiper
+    
+          style={{
+            "--swiper-theme-color":"#F2C12E",
+            "--swiper-pagination-color": "#F2C12E",
+            "--swiper-pagination-bullet-inactive-color": "#999999",
+            "--swiper-pagination-bullet-inactive-opacity": "1",
+            "--swiper-pagination-bullet-size": "16px",
+            "--swiper-pagination-bullet-border-radius": "0",
+            "--swiper-pagination-bullet-width": "40px",
+            "--swiper-pagination-bullet-height": "6px",
+            "--swiper-pagination-bullet-horizontal-gap" :" 15px",
+            "--swiper-navigation-gap" : "100px"
           }
-        </Swiper>
-      </section>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+          }  
+          autoplay ={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          grabCursor={true}
+          pagination={{
+            clickable:true
+          }}
+
+          navigation={{  //configurações do navi
+            enabled: false
+          }}
+
+          breakpoints={{
+            1281: {  //acima de 800px, aumenta os slides por visualização para dois, e ativa o navigation (as setinhas)
+              
+              navigation: {
+                enabled: true  
+              }}}}
+         
+          
+          
+          className={styles.swiperContainer}
+          
+
+      >
+        {data.map((item)=> (
+          
+        <SwiperSlide key={item.titulo} className={styles.swiperIndi}>
+          <CardPosts 
+            fileName={item.fileName}
+            imagem={item.imagemCapa}
+            titulo={item.titulo}
+            data={item.data}
+            previa={item.previa}
+            autor={item.autor}
+          />
+          <div className={styles.margin}>
+
+          </div>
+        </SwiperSlide>))
+        }
+      </Swiper>
+      
+      
+    </section>
+      
     </>
   );
 }
