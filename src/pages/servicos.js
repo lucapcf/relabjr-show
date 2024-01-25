954627bf97dee94bcee512b417a30f8f9bd778ce
lucapcf <lucapcf@gmail.com>
2024-01-24 21:11:41 -0300

    puxando titulo pagina do netlify

--- src/pages/servicos.js
+++ src/pages/servicos.js
@@ -48,7 +48,7 @@ export default function servicos({servicos, nav, foo, formularios}) {
   return (
     <>
       <Navbar  {...navData}/>
-      <Inicio title = {"SERVIÇOS"} image= {path}/>
+      <Inicio title = {servicos.titulo} image= {path}/>
       <TextoServicos {...inicioServicos}/>
       <EscoposServicos {...blocosServicos}/>
       <FormularioServicos {...formularios}/>
